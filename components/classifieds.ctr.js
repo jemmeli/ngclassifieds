8167bb39be768979a8043369e4a024888d03e94d
jemmeli <jemmeli84@gmail.com>
2020-01-14 00:49:24 +0100

    add a new classified

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -1,8 +1,18 @@
 (function(){
 	"use strict"
 
-	angular.module("ngClassifieds").controller("classifiedsCtrl", function($scope , $http, classifiedsFactory,$mdSidenav){
+	angular.module("ngClassifieds").controller("classifiedsCtrl", function($scope , $http, classifiedsFactory,$mdSidenav, $mdToast ){
 
+		classifiedsFactory.getClassifieds().then(function(classifieds){
+			$scope.classifieds = classifieds.data;
+			//console.log(classifieds.data);
+		});
+
+		var contact = {
+			name:"najmeddine",
+			phone : "(999) 99615 819",
+			email : "admin@admin.com"
+		}
 
 
 		$scope.openSidebar = function(){
@@ -12,15 +22,21 @@
 		$scope.closeSidebar = function(){
 			$mdSidenav('left').close();
 		}
+
+		$scope.saveClassified = function(classified) {
+			if (classified) {
+				classified.contact = contact;
+				$scope.classifieds.push(classified);
+				$scope.classified = {};//clear form after save
+				$scope.closeSidebar();//close sidebar after save
+				$mdToast.show(
+					$mdToast.simple().content("Classified saved!").position('top, right').hideDelay(3000)
+				);
+			}
+	      }
 		
 
-		classifiedsFactory.getClassifieds().then(function(classifieds){
-
-			$scope.classifieds = classifieds.data;
-			//console.log(classifieds.data);
-
-		});
-
+		
 
 
 	});
