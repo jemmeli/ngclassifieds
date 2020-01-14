f13993d09a1203d3237fa74c5c32672e9f84e2ff
jemmeli <jemmeli84@gmail.com>
2020-01-15 00:46:10 +0100

    create ui.router STATE

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,28 @@
-angular.module("ngClassifieds", ["ngMaterial"])
-.config(function($mdThemingProvider){
+angular.module("ngClassifieds", ["ngMaterial", "ui.router"])
+.config(function($mdThemingProvider, $stateProvider,$locationProvider){
+
+	$locationProvider.hashPrefix('');
+
+
 	$mdThemingProvider.theme('default')
 	.primaryPalette('teal')
 	.accentPalette('orange');
+
+
+	$stateProvider
+	.state('stateone', {
+		url:'/stateone',
+		template: '<h1>State one</h1>'
+	})
+	.state('statetwo', {
+		url:'/statetwo',
+		template: '<h1>State two</h1>'
+	});
+
 })
+
+
+
 
 .directive("helloWorld", function(){
 	return{
