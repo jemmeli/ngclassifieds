4ed81ed751b4777a82e16f7c24ed03c6e96d3d33
jemmeli <jemmeli84@gmail.com>
2020-01-15 20:13:49 +0100

    setup a card directive

---
+++ components/classifieds/card/classified-card.dir.js
@@ -0,0 +1,23 @@
+(function(){
+
+	"use strict";
+
+	angular.module("ngClassifieds").directive("classifiedCard", function(){
+
+		return : {
+			templateUrl : "components/ngClassifieds/card/classified-card.tpl.html",
+			scope:{
+
+			},
+			controller: classifiedCardController,
+			controllerAs: "vm"
+		}
+
+		function classifiedCardController(){
+			
+		}
+
+
+	})
+
+})();
