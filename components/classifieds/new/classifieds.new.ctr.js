565c559c8aaf25d620a2f32983d4598dd2610e28
jemmeli <jemmeli84@gmail.com>
2020-01-15 02:38:59 +0100

    using scope  watchers

--- components/classifieds/new/classifieds.new.ctr.js
+++ components/classifieds/new/classifieds.new.ctr.js
@@ -2,14 +2,27 @@
 
 	"use strict"
 
-	angular.module('ngClassifieds').controller('newClassifiedsCtrl', function(classifiedsFactory,$mdSidenav, $timeout, $mdDialog){
+	angular.module('ngClassifieds').controller('newClassifiedsCtrl', function($scope, $state , classifiedsFactory,$mdSidenav, $timeout, $mdDialog){
 
 		var vm = this;
+		vm.closeSidebar = closeSidebar;
 
 		//event loop problem in browser solving with settieout
 		$timeout(function(){
 			$mdSidenav('left').open();
-		})
+		});
+
+		$scope.$watch('vm.sidenavOpen', function(sidenav){
+			if (sidenav === false) {
+				$mdSidenav('left').close().then(function(){
+					$state.go('classifieds');//navigation 
+				});
+			}
+		});
+
+		function closeSidebar(){
+			vm.sidenavOpen = false;
+		}
 		
 
 	})
