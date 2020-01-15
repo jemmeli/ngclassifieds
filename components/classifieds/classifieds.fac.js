b1bd3cecff20a95c60ec9ec33e3e455a6cdb103a
jemmeli <jemmeli84@gmail.com>
2020-01-15 18:25:08 +0100

    reading from firebase

--- components/classifieds/classifieds.fac.js
+++ components/classifieds/classifieds.fac.js
@@ -4,7 +4,7 @@
 
 	angular.module("ngClassifieds").factory("classifiedsFactory" , function($http, $firebaseArray){
 
-		//var ref = new Firebase('https://ngclassifieds-c7a55.firebaseio.com/');
+		
 		 var ref = firebase.database().ref();
 
 		return{
